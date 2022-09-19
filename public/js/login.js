6b0a13b266b31c9d08a444d7c58cf1cdb11ebf4d
Jennyfer Constanza <jennyferislas@gmail.com>
2022-09-18 22:59:25 -0700

    update

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,25 @@
+const { response } = require("express");
+
 async function loginForm(event) {
     event.preventDefault();
+
+    const email = document.querySelector("#emailLogin").value.trim();
+    const password = document.querySelector("#passwordLogin").value.trim();
+
+    if(email && password) {
+        const response = await fetch("/api/users/login", {
+            method:"post",
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {"ContentType": "application/json"}
+        });
+        if(response.ok) {
+            document.location.replace("/userDashboard");
+        } else {
+            alert(response.postTextUpdate);
+        }
+    }
 }
+document.querySelector("loginForm").addEventListener("submit", loginForm);
