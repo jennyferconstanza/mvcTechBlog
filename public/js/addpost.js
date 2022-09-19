75c329b1976992ba51e175d1ced09d10fbeaa37d
Jennyfer Constanza <jennyferislas@gmail.com>
2022-09-18 22:39:51 -0700

    update handlebars and js

---
+++ public/js/addpost.js
@@ -0,0 +1,21 @@
+async function newForm(event) {
+    event.preventDefault();
+    const title = document.querySelector("input[name='newPostTitle']").value;
+    const postText = document.querySelector("textarea[name='postText']").value;
+    const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+        title, 
+        postText
+        }),
+        headers: {
+            "contentType": "application/json"
+        }
+    });
+    if (response.ok){
+    document.location.replace("/userDashboard");
+    } else {
+        alert(response.postTextUpdate);
+    }
+}
+document.querySelector(".newPostForm").addEventListener("submite", newForm)
---
+++ public/js/login.js
@@ -0,0 +1,3 @@
+async function loginForm(event) {
+    event.preventDefault();
+}
