1e8703b182d096ee820b6639b947f59ff1cc5de8
Jennyfer Constanza <jennyferislas@gmail.com>
2022-09-20 00:10:41 -0700

    update models

--- models/Post.js
+++ models/Post.js
@@ -4,33 +4,40 @@ const sequelize = require("../config/connection");
 class Post extends Model {}
 
 Post.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: "id"
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true, 
-        underscored: true,
-        modelName: "post"
-    }
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    post_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
 );
-model.exports = Post;
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
+const Textarea = require("./Textarea");
+
 // user has many posts
 User.hasMany(Post, {
     foreignKey: "user_id"
@@ -8,4 +10,25 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: "user_id"
 });
-module.exports = { User, Post };
+
+// textarea belongs to one user
+Textarea.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+// textarea belongs to one user
+Textarea.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+// user makes many text inputs
+User.hasMany(Textarea, {
+    foreignKey: "user_id"
+});
+
+// user makes many posts
+Post.hasMany(Textarea, {
+    foreignKey: "post_id"
+});
+
+module.exports = { User, Post, Textarea };
